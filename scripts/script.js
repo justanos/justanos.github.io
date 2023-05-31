694a131175d1edb06d3a63e017e52623a94d8f57
justanos <mauromathisos@gmail.com>
2023-05-31 16:48:42 +0200

    More updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
-document.getElementById('darkModeSwitch').addEventListener('change', function() {
-  document.body.classList.toggle('dark-mode');
-});
+window.onload = function() {
+  var checkbox = document.getElementById('darkModeSwitch');
+
+  // Retrieve saved mode from localStorage
+  var savedMode = localStorage.getItem('darkMode');
+  if (savedMode === 'true') {
+    document.body.classList.add('dark-mode');
+    checkbox.checked = true;
+  } else {
+    document.body.classList.remove('dark-mode');
+    checkbox.checked = false;
+  }
+
+  checkbox.addEventListener('change', function() {
+    if (this.checked) {
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('darkMode', 'true');
+    } else {
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('darkMode', 'false');
+    }
+  });
+};
