e30447fd9ba7dfc46b3f791e7b59f493d4977986
justanos <135068252+justanos@users.noreply.github.com>
2023-06-04 16:00:27 +0200

    Update adblock.js

--- scripts/adblock.js
+++ scripts/adblock.js
@@ -1,1 +1,10 @@
-
+<div class="ad-box" style="position:fixed;top:0;left:0;" aria-hidden="true">
+  &nbsp;
+</div>
+<script>
+  setTimeout(function () {
+    var adBoxEl = document.querySelector(".ad-box")
+    var hasAdBlock = window.getComputedStyle(adBoxEl)?.display === "none"
+    // ... save to DB
+  }, 2000)
+</script>
