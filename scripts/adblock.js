d34d8fecaecf8b8237e5eaf936a25a2ef6ca2628
justanos <135068252+justanos@users.noreply.github.com>
2023-06-04 14:36:22 +0200

    Update adblock.js
    test

--- scripts/adblock.js
+++ scripts/adblock.js
@@ -14,3 +14,36 @@ function adBlockCheck() {
 
 // 2) Call the function:
 adBlockCheck()
+function adBlockCheck() {
+    setTimeout(function() {
+        document.body.innerHTML += `<div class="adsbygoogle" id="test-ad"></div>`;
+        const testAd = document.getElementById('test-ad');
+        const testAdStyle = getComputedStyle(testAd);
+
+        if (testAdStyle.display === 'none') {	
+            const adContainer = document.getElementById('ad-container');
+            
+            const myImg = document.createElement('img');
+            myImg.src = 'https://your-imgage-url.com/image.png';
+            adContainer.append(myImg);
+        } 
+    },1000)
+}
+function adBlockCheck() {
+    setTimeout(function() {
+        document.body.innerHTML += `<div class="adsbygoogle" id="test-ad"></div>`;
+        const testAd = document.getElementById('test-ad');
+        const testAdStyle = getComputedStyle(testAd);
+
+        if (testAdStyle.display === 'none') {	
+            const banner = document.createElement('div');
+            banner.innerHTML = `
+            <h3>Ad blocker detected</h3>
+            <p>To keep our services free, we use advertising on this site. Please consider supporting us by disabling your ad blocker.</p>
+            `
+            banner.setAttribute('class', 'alert');
+            banner.style = "background: red; color: whitesmoke; width: 100%; padding: 0.8rem; font-size: 1.2rem";
+            document.body.prepend(banner);
+        } 
+    },1000)
+}
