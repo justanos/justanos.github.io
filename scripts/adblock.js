a28c741a11cac53c4dd6015246c3c62902721bf8
justanos <135068252+justanos@users.noreply.github.com>
2023-06-04 14:34:47 +0200

    Update adblock.js

--- scripts/adblock.js
+++ scripts/adblock.js
@@ -1,1 +1,16 @@
+function adBlockCheck() {
+    setTimeout(function() {
+        document.body.innerHTML += `<div class="adsbygoogle" id="test-ad"></div>`;
+        const testAd = document.getElementById('test-ad');
+        const testAdStyle = getComputedStyle(testAd);
 
+        if (testAdStyle.display === 'none') {	
+            // Adblock enabled: do something
+        } else {	
+            // Adblock disabled: do something
+        }
+    },1000)
+}
+
+// 2) Call the function:
+adBlockCheck()
