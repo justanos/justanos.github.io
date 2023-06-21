6c1ebb37bd19ce6d95a480a6016489f389ea955e
justanos <135068252+justanos@users.noreply.github.com>
2023-06-21 15:04:31 +0200

    Update adblock.js

--- scripts/adblock.js
+++ scripts/adblock.js
@@ -1,9 +1,19 @@
-setTimeout(function() {
-fetch('https://www3.doubleclick.net', {
-method: "HEAD",
-mode: "no-cors",
-cache: "no-store",
-}).catch(()=>{
-alert("detect AdBlock")
-});
-}, 3500);
+<script>
+  var ad = document.createElement('div');
+  ad.innerHTML = '&nbsp;';
+  ad.className = 'ad';
+  ad.style.width = '1px';
+  ad.style.height = '1px';
+  ad.style.display = 'none';
+  document.body.appendChild(ad);
+
+  // Check if the dummy ad element is blocked
+  window.setTimeout(function() {
+    if (ad.offsetHeight === 0) {
+      // Ad-blocker detected
+      // Display your message or take appropriate action
+      alert('Please disable your ad-blocker to support our website.');
+    }
+    document.body.removeChild(ad);
+  }, 100);
+</script>
