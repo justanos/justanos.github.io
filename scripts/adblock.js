f48ee8ff84fd8ed7a7a9a56c1b6d5c3769d9aba3
justanos <135068252+justanos@users.noreply.github.com>
2023-06-21 15:10:43 +0200

    Update adblock.js

--- scripts/adblock.js
+++ scripts/adblock.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var adblockMessage = document.getElementById('adblock-message');
-  var ad = document.createElement('div');
-  ad.innerHTML = '&nbsp;';
-  ad.className = 'ad';
-  ad.style.width = '1px';
-  ad.style.height = '1px';
-  ad.style.display = 'none';
-  document.body.appendChild(ad);
-
-  window.setTimeout(function() {
-    if (ad.offsetHeight === 0) {
-      adblockMessage.style.display = 'block';
-    }
-    document.body.removeChild(ad);
-  }, 100);
-});
 
