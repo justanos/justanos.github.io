e31f51e663adf33c8fbf082ff09a0e841246c2c0
justanos <135068252+justanos@users.noreply.github.com>
2023-06-21 15:06:04 +0200

    Update adblock.js

--- scripts/adblock.js
+++ scripts/adblock.js
@@ -1,19 +1,1 @@
-<script>
-  var ad = document.createElement('div');
-  ad.innerHTML = '&nbsp;';
-  ad.className = 'ad';
-  ad.style.width = '1px';
-  ad.style.height = '1px';
-  ad.style.display = 'none';
-  document.body.appendChild(ad);
 
-  // Check if the dummy ad element is blocked
-  window.setTimeout(function() {
-    if (ad.offsetHeight === 0) {
-      // Ad-blocker detected
-      // Display your message or take appropriate action
-      alert('Please disable your ad-blocker to support our website.');
-    }
-    document.body.removeChild(ad);
-  }, 100);
-</script>
